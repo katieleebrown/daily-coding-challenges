9419e5ab5c88b131179a758eafcdc3ef9eb48f33
Katie Brown <78991257+katieleebrown@users.noreply.github.com>
2022-03-16 12:59:48 -0400

    Delete Daily Coding Challenges directory

--- Daily Coding Challenges/beginnerSeriesSchoolPaperwork.js
+++
@@ -1,9 +0,0 @@
-// Beginner Series #1 School Paperwork Codewars Fundamentals
-// https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd
-function paperwork(n, m) {
-    if (n > 0 && m > 0) {
-        return n * m
-    } else if (n <= 0 || m <= 0) {
-        return 0
-    }
-}
--- Daily Coding Challenges/multiply.js
+++
@@ -1,6 +0,0 @@
-// Multiply
-// https://www.codewars.com/kata/50654ddff44f800200000004
-
-function multiply(a, b) {
-    return a * b
-}
