0059419b70ce21f014837533e25648f91624eb1d
Katie Brown <78991257+katieleebrown@users.noreply.github.com>
2022-03-16 13:19:08 -0400

    Update beginnerReduceButGrow.js

--- beginnerReduceButGrow.js
+++ beginnerReduceButGrow.js
@@ -1,5 +1,5 @@
-Beginner - Reduce but Grow
-https://www.codewars.com/kata/57f780909f7e8e3183000078/solutions/javascript
+// Beginner - Reduce but Grow
+// https://www.codewars.com/kata/57f780909f7e8e3183000078/solutions/javascript
 
 function grow(x) {
     let product = 1
