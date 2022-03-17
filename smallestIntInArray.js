260536fe04b66c8a2764a1f0355f9dc7d9493a50
Katie Brown <78991257+katieleebrown@users.noreply.github.com>
2022-03-17 13:48:19 -0400

    Update smallestIntInArray.js

--- smallestIntInArray.js
+++ smallestIntInArray.js
@@ -15,3 +15,18 @@ class SmallestIntegerFinder {
         return smallestInt
     }
 }
+
+// Edge case - this does not work if 0 is both the smallest integer and not last in the array. Here is my updated answer after finding this error:
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+        let smallestInt = Number.POSITIVE_INFINITY
+
+        for (let i = 0; i < args.length; i++) {
+            if (args[i] < smallestInt) {
+                smallestInt = args[i]
+            }
+        }
+        return smallestInt
+    }
+}
