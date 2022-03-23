928b6b888fff3049cb5f8dbe1647ef3a925fb84e
Katie Brown <78991257+katieleebrown@users.noreply.github.com>
2022-03-23 13:34:42 -0400

    Add files via upload

---
+++ countTheMonkeys.js
@@ -0,0 +1,10 @@
+// Count the Monkeys
+// https://www.codewars.com/kata/56f69d9f9400f508fb000ba7
+
+function monkeyCount(n) {
+    let monkeys = []
+    for (let i = 1; i <= n; i++) {
+        monkeys.push(i)
+    }
+    return monkeys
+}
