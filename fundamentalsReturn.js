ba6dce6e688089a5f7356b8e61c128575c67f139
Katie Brown <katieleebrown19@gmail.com>
2022-04-11 13:10:01 -0400

    challenges for Monday, 4/11

---
+++ fundamentalsReturn.js
@@ -0,0 +1,26 @@
+// Fundamentals: return 
+// https://www.codewars.com/kata/55a5befdf16499bffb00007b
+
+function add(a, b) {
+    return a + b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function mod(a, b) {
+    return a % b
+}
+
+function exponent(a, b) {
+    return a ** b
+}
+
+function subt(a, b) {
+    return a - b
+}
