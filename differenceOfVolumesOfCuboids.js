6e8fa1f3a3a2404631d2f6f137445b7f45d66c28
katieleebrown <katieleebrown19@gmail.com>
2022-05-05 14:03:41 -0400

    challenges for Thursday, 5/5

---
+++ differenceOfVolumesOfCuboids.js
@@ -0,0 +1,6 @@
+// Difference of Volumes of Cuboids
+// https://www.codewars.com/kata/58cb43f4256836ed95000f97
+
+function findDifference(a, b) {
+    return Math.abs((a[0]*a[1]*a[2]) - (b[0]*b[1])*b[2])
+  }
