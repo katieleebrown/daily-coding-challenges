7acb1ba0b171297f8493b5f8d4cfac51c9950546
katieleebrown <katieleebrown19@gmail.com>
2022-05-05 13:54:56 -0400

    challenges for Wednesday, 5/4

---
+++ unfinishedLoopBugFixing1.js
@@ -0,0 +1,12 @@
+// Unfinished Loop - Bug Fixing # 1
+// 
+
+function createArray(number){
+    var newArray = [];
+    
+    for(var counter = 1; counter <= number; counter++){
+      newArray.push(counter);
+    }
+    
+    return newArray;
+  }
