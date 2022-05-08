a830cad074b91824d26bb2bfb7e4a3bac58fd37c
katieleebrown <katieleebrown19@gmail.com>
2022-05-08 11:10:15 -0400

    challenges for Sunday, 5/8

---
+++ keepUpTheHoop.js
@@ -0,0 +1,6 @@
+// Keep Up the Hoop
+// https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145
+
+function hoopCount (n) {
+    return n >= 10 ? 'Great, now move on to tricks' : 'Keep at it until you get it'
+ }
