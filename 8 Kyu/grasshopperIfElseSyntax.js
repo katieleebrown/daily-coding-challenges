39a9642f65cda6225f1050321b53704e3d8a9ca3
katieleebrown <katieleebrown19@gmail.com>
2022-05-09 08:47:02 -0400

    challenges for Monday, 5/9

---
+++ 8 Kyu/grasshopperIfElseSyntax.js
@@ -0,0 +1,10 @@
+// Grasshopperr - If/Else Syntax Bug
+// https://www.codewars.com/kata/57089707fe2d01529f00024a
+
+function checkAlive(health) {
+    if (health <= 0) {
+      return false
+    } else {
+      return true
+    }
+  }
---
+++ 8 Kyu/grasshopperMessiGoals.js
@@ -0,0 +1,6 @@
+// Grasshopper - Messi Goals Function 
+// https://www.codewars.com/kata/55f73be6e12baaa5900000d4
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+  }
