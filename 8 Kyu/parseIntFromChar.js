878331a2a7e9264cd18372332112cc363e21ba96
katieleebrown <katieleebrown19@gmail.com>
2022-05-17 13:55:49 -0400

    challenges for Tuesday, 5/17

---
+++ 8 Kyu/parseIntFromChar.js
@@ -0,0 +1,7 @@
+// Parse Int from Character Problem
+// https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1
+
+function getAge(inputString){
+    let arr = inputString.split('')
+    return Number(arr[0])
+  }
