b1606ec29d8b4037d382f0a5f4072bbdd7583ae0
katieleebrown <katieleebrown19@gmail.com>
2022-05-20 13:01:16 -0400

    challenges for Friday, 5/20

---
+++ 7 Kyu/descendingOrder.js
@@ -0,0 +1,6 @@
+// Descending Order
+// https://www.codewars.com/kata/5467e4d82edf8bbf40000155
+
+function descendingOrder(n){
+    return Number(n.toString().split('').sort((a, b) => b - a).join(''))
+  }
