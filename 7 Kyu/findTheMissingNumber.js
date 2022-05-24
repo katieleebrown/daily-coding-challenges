165a59743d8eed39b60f19efd703d84c1a61d8c2
Katie Brown <katieleebrown19@gmail.com>
2022-05-24 13:29:08 -0400

    challenges for Tuesday, 5/24

---
+++ 7 Kyu/findTheMissingNumber.js
@@ -0,0 +1,11 @@
+// Find the Missing Number
+// https://www.codewars.com/kata/57f5e7bd60d0a0cfd900032d
+
+function missingNo(nums) {
+    let sorted = nums.sort((a, b) => a - b)
+    for (let i = 0; i < sorted.length; i++) {
+        if (sorted[i] + 1 !== sorted[i + 1]) {
+            return sorted[i] + 1
+        }
+    }
+}
