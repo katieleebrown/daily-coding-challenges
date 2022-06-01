496463e5cea48d1289609daa1adc41b27af16f80
Katie Brown <katieleebrown19@gmail.com>
2022-06-01 13:29:31 -0400

    challenges for Wednesday, 6/1

---
+++ 6 Kyu/createPhoneNumber.js
@@ -0,0 +1,6 @@
+// Create Phone Number
+// https://www.codewars.com/kata/525f50e3b73515a6db000b83
+
+function createPhoneNumber(numbers) {
+    return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+}
