438a29142247fca8a2706290ca821cac9af4159d
katieleebrown <katieleebrown19@gmail.com>
2022-06-09 13:55:05 -0400

    challenges for Thursday, 6/9

---
+++ 8 Kyu/trainingIfElseTernary.js
@@ -0,0 +1,6 @@
+// Training JS # 7: If..Else and Ternary Operator
+// https://www.codewars.com/kata/57202aefe8d6c514300001fd
+
+function saleHotdogs(n){
+    return n < 5 ? n * 100 : n >= 10 ? n * 90 : n * 95
+  }
