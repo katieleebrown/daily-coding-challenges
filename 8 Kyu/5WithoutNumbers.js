b8efd4e5314f7466a5918bf34e4311c1682e7170
Katie Brown <katieleebrown19@gmail.com>
2022-06-16 14:09:43 -0400

    challenges for Thursday, 6/16

---
+++ 8 Kyu/5WithoutNumbers.js
@@ -0,0 +1,7 @@
+// 5 Without Numbers
+// https://www.codewars.com/kata/59441520102eaa25260000bf
+
+function unusualFive() {
+    let arr = [true, true, true, true, true]
+    return arr.length
+}
---
+++ 8 Kyu/theWideMouthedFrog.js
@@ -0,0 +1,6 @@
+// The Wide Mouthed Frog
+// https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89
+
+function mouthSize(animal) {
+    return animal.toLowerCase() == 'alligator' ? 'small' : 'wide'
+}
