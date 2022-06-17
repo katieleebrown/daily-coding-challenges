8f4bebd170c92738017cb9507e6d0e5e8e07780f
katieleebrown <katieleebrown19@gmail.com>
2022-06-17 14:47:15 -0400

    challenges for Friday, 6/17

---
+++ 8 Kyu/determineOffspring.js
@@ -0,0 +1,6 @@
+// Determine Offspring
+// https://www.codewars.com/kata/56530b444e831334c0000020
+
+function chromosomeCheck(sperm) {
+    return sperm == 'XX' ? "Congratulations! You're going to have a daughter." : "Congratulations! You're going to have a son."
+  }
