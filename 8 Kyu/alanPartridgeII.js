b8fdc95e78500aff2da05e59f56e880e168f050f
katieleebrown <katieleebrown19@gmail.com>
2022-06-19 16:30:37 -0400

    challenges for Sunday, 6/19

---
+++ 8 Kyu/alanPartridgeII.js
@@ -0,0 +1,6 @@
+// Alan Partridge II - Apple Turnover
+// https://www.codewars.com/kata/580a094553bd9ec5d800007d
+
+function apple(x){
+    return x ** 2 > 1000 ? 'It\'s hotter than the sun!!' : 'Help yourself to a honeycomb Yorkie for the glovebox.'
+  }
