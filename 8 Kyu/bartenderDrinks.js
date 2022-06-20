66009136283dc46dbb0b1537591879937145a1a5
katieleebrown <katieleebrown19@gmail.com>
2022-06-20 15:56:32 -0400

    challenges for Mon, 6/20

---
+++ 8 Kyu/bartenderDrinks.js
@@ -0,0 +1,27 @@
+// L1: Bartender, Drinks!
+// https://www.codewars.com/kata/568dc014440f03b13900001d
+
+function getDrinkByProfession(param){
+    switch(param.toLowerCase()) {
+        case 'jabroni':
+          return 'Patron Tequila'
+          break
+        case 'school counselor':
+          return 'Anything with Alcohol'
+          break
+        case 'programmer':
+          return 'Hipster Craft Beer'
+          break
+        case 'bike gang member':
+          return 'Moonshine'
+          break
+        case 'politician':
+          return 'Your tax dollars'
+          break
+        case 'rapper':
+          return 'Cristal'
+          break
+        default:
+          return 'Beer'
+    }
+  }
---
+++ 8 Kyu/reversingWordsInAString.js
@@ -0,0 +1,6 @@
+// Reversing Words in a String
+// https://www.codewars.com/kata/57a55c8b72292d057b000594
+
+function reverse(string){
+    return string.split(' ').reverse().join(' ')
+  }
