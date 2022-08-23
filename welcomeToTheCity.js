7a24f753020ad45955d9b82bc8d60ffb04424478
Katie Brown <katieleebrown19@gmail.com>
2022-08-23 12:52:32 -0400

    challenges for Tues, 8/23

---
+++ welcomeToTheCity.js
@@ -0,0 +1,6 @@
+// Welcome to the City
+// https://www.codewars.com/kata/5302d846be2a9189af0001e4
+
+function sayHello(name, city, state) {
+    return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
+}
