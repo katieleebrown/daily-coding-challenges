899d2b9db67cbf9f95238e7660de0d913ce37ffd
Katie Brown <katieleebrown19@gmail.com>
2022-08-23 12:46:14 -0400

    challenges for Tues, 8/23

---
+++ takeTheDerivative.js
@@ -0,0 +1,6 @@
+// Take the Derivative
+// https://www.codewars.com/kata/5963c18ecb97be020b0000a2
+
+function derive(coefficient, exponent) {
+    return `${coefficient * exponent}x^${exponent - 1}`
+}
