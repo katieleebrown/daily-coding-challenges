4b1c230aa9c2e0c292f470f476a89a8658bf3f13
Katie Brown <katieleebrown19@gmail.com>
2022-09-08 11:02:42 -0400

    challenges for Thur, 9/8

---
+++ 8 Kyu/takeAnArrowToTheKnee.js
@@ -0,0 +1,6 @@
+// Take an Arrow to the Knee, Functionally
+// https://www.codewars.com/kata/559f3123e66a7204f000009f
+
+var ArrowFunc = function (arr) {
+    return arr.map(x => String.fromCharCode(x)).join(''); //Complete this function
+}
