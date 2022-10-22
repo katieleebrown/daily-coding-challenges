0fefcc25fbc66efda174804b0319ef42c98f854a
katieleebrown <katieleebrown19@gmail.com>
2022-10-22 11:43:03 -0400

    challenges for 10/22

---
+++ Codewars/7 Kyu/trainingJS5.js
@@ -0,0 +1,6 @@
+// Training JS #5: Basic Data Types: Objects
+// https://www.codewars.com/kata/571f1eb77e8954a812000837/
+
+function animal(obj){
+    return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`
+  }
