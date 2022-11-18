48a0c427e6cd48465af4525e57ddadcfebfb3c9b
Katie Brown <katieleebrown19@gmail.com>
2022-11-18 10:04:59 -0500

    challenges for 11/18

---
+++ Codewars/8 Kyu/drinkAbout.js
@@ -0,0 +1,6 @@
+// Drink About
+// https://www.codewars.com/kata/56170e844da7c6f647000063
+
+function peopleWithAgeDrink(old) {
+    return old < 14 ? 'drink toddy' : old < 18 ? 'drink coke' : old < 21 ? 'drink beer' : 'drink whisky'
+};
