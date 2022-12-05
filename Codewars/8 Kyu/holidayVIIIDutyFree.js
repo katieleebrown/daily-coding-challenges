60c5873786ede9d35e2a72938c9752c9da50e1b9
Katie Brown <katieleebrown19@gmail.com>
2022-12-05 16:18:14 -0500

    challenges for 12/5

---
+++ Codewars/8 Kyu/holidayVIIIDutyFree.js
@@ -0,0 +1,6 @@
+// Holiday VIII - Duty Free
+// https://www.codewars.com/kata/57e92e91b63b6cbac20001e5
+
+function dutyFree(normPrice, discount, hol) {
+    return Math.floor(hol / (normPrice * (discount / 100)))
+}
