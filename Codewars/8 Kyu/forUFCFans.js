fb3a29d4ec6706cf7a4b39d47432883a0da81292
Katie Brown <katieleebrown19@gmail.com>
2023-01-11 16:20:04 -0500

    challenges for 1/11

---
+++ Codewars/8 Kyu/forUFCFans.js
@@ -0,0 +1,8 @@
+// For UFC Fans
+// https://www.codewars.com/kata/582dafb611d576b745000b74/
+
+var quote = function (fighter) {
+    let winner = fighter.toLowerCase()
+
+    return winner == 'george saint pierre' ? 'I am not impressed by your performance.' : `I'd like to take this chance to apologize.. To absolutely NOBODY!`
+};
