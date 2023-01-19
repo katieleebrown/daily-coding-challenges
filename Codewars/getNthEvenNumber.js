0815e7120fdefde339ea5b54aadb02b8c2688731
katieleebrown <katieleebrown19@gmail.com>
2023-01-19 11:22:45 -0500

    challenges for 1/19

---
+++ Codewars/getNthEvenNumber.js
@@ -0,0 +1,18 @@
+// Get Nth Even Number
+// https://www.codewars.com/kata/5933a1f8552bc2750a0000ed
+
+function nthEven(n){
+    // Quick Math Version
+    return (n * 2) - 2 
+    
+  //   Code Version
+  //   let count = 1
+  //   let even = 0
+    
+  //   while(count < n) {
+  //     count += 1
+  //     even += 2
+  //   }
+    
+  //   return even
+  }
